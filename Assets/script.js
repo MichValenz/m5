aa9e6a4e70f81b77f0784211b553bb1d6b7b5798
906284 <michaelcounseller@gmail.com>
2021-12-09 14:55:38 -0600

    thought async/await and promise/then mattered in this case but it doesnt, for clarity changed it back to promise/then because i understand it better

--- Assets/script.js
+++ Assets/script.js
@@ -21,45 +21,50 @@ function searchActor() {
 
 searchActor();
 
-async function getActor(actor) {
-
+function getActor(actor) {
+    
     let actorAPI = `https://imdb-api.com/en/API/SearchName/k_a65zgvjy/${actor}`;
 
-    const response = await fetch(actorAPI)
+    fetch(actorAPI).then(function (response) {
 
-    const data = await response.json();
+        if(response.ok) {
 
-    const results = data.results;
+            response.json().then(function (data) {
 
-    //console.log(results);
-    results.forEach(result => {
+                const results = data.results;
 
-        //Checks each result array and the conditios HAS to include Actor OR Actress
-        if (result.description.includes('Actor') || result.description.includes('Actress')) {
-
-            let actorData = result.description
-
-            //Will seperate every single character into a seperate string
-            getActorDataValues = Object.values(actorData)
-
-            /* console.log(getActorDataValues) */
-
-            //Will join all the values so its one giant string, but then splitting every word in an array
-            let stringifyActorData = getActorDataValues.join("").split(" ")
+                //console.log(results);
+                results.forEach(result => {
             
-            //An array of words I want to remove from the data we're getting
-            const removeIrrelevantWords = ['(I)', '(II)', '(III)', '(Actor,', '(Actress,']
-
-            //Filtering out words we don't need, checks if the stingifyActorData has the words we don't want. When it finds we don't want it will remove them
-            stringifyActorData = stringifyActorData.filter(checkedData => removeIrrelevantWords.includes(checkedData) === false)
-
-            //This is the variable we want to pass to the Youtube API for it to search
-            newActorData = stringifyActorData.toString();
-
-            console.log(newActorData)
-        }
-        //If we want to catch an error, we can just put this into a try block try {}
+                    //Checks each result array and the conditios HAS to include Actor OR Actress
+                    if (result.description.includes('Actor') || result.description.includes('Actress')) {
+            
+                        let actorData = result.description
+            
+                        //Will seperate every single character into a seperate string
+                        getActorDataValues = Object.values(actorData);
+            
+                        /* console.log(getActorDataValues) */
+            
+                        //Will join all the values so its one giant string, but then splitting every word in an array
+                        let stringifyActorData = getActorDataValues.join("").split(" ");
+                        
+                        //An array of words I want to remove from the data we're getting
+                        const removeIrrelevantWords = ['(I)', '(II)', '(III)', '(Actor,', '(Actress,'];
+            
+                        //Filtering out words we don't need, checks if the stingifyActorData has the words we don't want. When it finds we don't want it will remove them
+                        stringifyActorData = stringifyActorData.filter(checkedData => removeIrrelevantWords.includes(checkedData) === false);
+            
+                        //This is the variable we want to pass to the Youtube API for it to search
+                        newActorData = stringifyActorData.toString();
+            
+                        console.log(newActorData);
+                    }
+                    //If we want to catch an error, we can just put this into a try block try {}
+                });
+            });
+        };
     });
-}
+};
 
 
