0a68affd2cbc4c3425a54d4cfbd955446e1da3db
906284 <michaelcounseller@gmail.com>
2021-12-10 02:41:18 -0600

    added working modal for when the entry is not a proper actor, such as blank or numbers

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,7 @@
-let searchForm = document.getElementById('search-form')
-let searchInput = document.getElementById('textarea-input')
+const searchForm = document.getElementById('search-form')
+const searchInput = document.getElementById('textarea-input')
+
+const actorModal = document.getElementById('actor-modal-warning')
 
 function searchActor() {
 
@@ -7,17 +9,48 @@ function searchActor() {
 
         event.preventDefault();
 
-        let actor = searchInput.value
+        let actor = searchInput.value;
 
-        console.log(actor)
-
-        if (actor) {
+        console.log();
+        //isNaN is basically, is Not a Number. Checking if actor is not a number, otherwise will be invalid and will not take.
+        if (isNaN(actor)) {
 
             getActor(actor);
-        }
 
+        } else if (actor === "") {
+
+            triggerActorModal();
+
+            console.log('is blank')
+
+        } else {
+
+            triggerActorModal();
+
+            console.log('is a number')
+
+        };
     });
 }
+
+function triggerActorModal() {
+
+    actorModal.classList.add('is-active');
+
+};
+
+function closeOpenModal() {
+
+    const closeModal = document.querySelector('.modal-close');
+
+    closeModal.addEventListener('click', function () {
+
+        actorModal.classList.remove('is-active');
+    });
+
+};
+
+closeOpenModal();
 
 searchActor();
 
@@ -29,7 +62,7 @@ function getActor(actor) {
 
     fetch(actorAPI).then(function (response) {
 
-        if(response.ok) {
+        if (response.ok) {
 
             response.json().then(function (data) {
 
@@ -37,29 +70,29 @@ function getActor(actor) {
 
                 //console.log(results);
                 results.forEach(result => {
-            
+
                     //Checks each result array and the conditios HAS to include Actor OR Actress
                     if (result.description.includes('Actor') || result.description.includes('Actress')) {
-            
+
                         let actorData = result.description
-            
+
                         //Create an array, from an object. So then we can use other syntax like join, split, filter etc. Will also seperate every single character into a seperate string
                         getActorDataValues = Object.values(actorData);
-            
+
                         /* console.log(getActorDataValues) */
-            
+
                         //Will join all the values so its one giant string, but then splitting every word in an array
                         let stringifyActorData = getActorDataValues.join("").split(" ");
-                        
+
                         //An array of words I want to remove from the data we're getting
-                        const removeIrrelevantWords = ['(I)', '(II)', '(III)', '(Actor,', '(Actress,'];
-            
+                        const removeIrrelevantWords = ['(I)', '(II)', '(III)', '(IV)', '(V)', '(VI)', '(Actor,', '(Actress,'];
+
                         //Filtering out words we don't need, checks if the stingifyActorData has the words we don't want. When it finds we don't want it will remove them
                         stringifyActorData = stringifyActorData.filter(checkedData => removeIrrelevantWords.includes(checkedData) === false);
-            
+
                         //This is the variable we want to pass to the Youtube API for it to search
                         newActorData = stringifyActorData.toString();
-            
+
                         console.log(newActorData);
                     };
                 });
